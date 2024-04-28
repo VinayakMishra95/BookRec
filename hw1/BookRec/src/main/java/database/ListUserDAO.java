fca6d91e4ab8400a0a28618e605818a863b81749
vinayak_bitbucket22 <vinayak.kumar@studenti.unipd.it>
2024-04-28 13:51:25 +0200

    List User Rest API template added

---
+++ hw1/BookRec/src/main/java/database/ListUserDAO.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018-2023 University of Padua, Italy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unipd.webapp.project.database;
+
+import unipd.webapp.project.resource.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Lists all the employees in the database.
+ *
+ * @author Nicola Ferro (ferro@dei.unipd.it)
+ * @version 1.00
+ * @since 1.00
+ */
+public final class ListUserDAO extends AbstractDAO<List<User>> {
+
+    /**
+     * The SQL statement to be executed
+     */
+    private static final String STATEMENT = "SELECT name,email FROM Public.User";
+
+    /**
+     * Creates a new object for listing all the employees.
+     *
+     * @param con the connection to the database.
+     */
+    public ListUserDAO(final Connection con) {
+        super(con);
+    }
+
+    @Override
+    protected final void doAccess() throws SQLException {
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        // the results of the search
+        final List<User> users = new ArrayList<User>();
+
+        try {
+            pstmt = con.prepareStatement(STATEMENT);
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                users.add(new User(rs.getString("name"), "email@empty.com", "password"));
+            }
+
+            LOGGER.info("User(s) successfully listed.");
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
+
+        outputParam = users;
+    }
+}
